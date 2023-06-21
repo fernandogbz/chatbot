df652406c87f394024b642053efd42013a2ba0a8
Fernando Jimenez <fgjv06@gmail.com>
2023-06-21 00:08:26 -0400

    JS - Create a chat <li> element with passed message and className

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const sendChatBtn = document.querySelector(".chat-input span");
 let userMessage;
 
 const createChatLi = (message, className) => {
+  // Create a chat <li> element with passed message and className
   const chatLi = document.createElement("li");
   chatLi.classList.add("chat", className);
   let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
   chatLi.innerHTML = chatContent;
-  
+  return chatLi;
 }
 
 const handleChat = () => {

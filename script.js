5414892282418cc8e96d0ddd4116d99e0cd607c7
Fernando Jimenez <fgjv06@gmail.com>
2023-06-21 00:07:30 -0400

    JS - write chatContent in the html

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ let userMessage;
 const createChatLi = (message, className) => {
   const chatLi = document.createElement("li");
   chatLi.classList.add("chat", className);
-  let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`
+  let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
+  chatLi.innerHTML = chatContent;
+  
 }
 
 const handleChat = () => {
